c2115f2987f0f7998ae4c5f10042a7037383a1e6
mnkcoders <jaume.llopis1@gmail.com>
2020-03-01 05:31:06 +0100

    Fixes

--- coders-digital-creator.php
+++ coders-digital-creator.php
@@ -2,69 +2,36 @@
 /*******************************************************************************
  * Plugin Name: Coders Digital Creator
  * Plugin URI: https://coderstheme.org
- * Description: Digital Creator for Coders Framework
+ * Description: Test App for Coders Framework
  * Version: 1.0.0
- * Author: Jaume Llopis
+ * Author: Coder1
  * Author URI: 
  * License: GPLv2 or later
- * Text Domain: coders_digital_creator
- * Class: CodersDigitalCreator
+ * Text Domain: coders_tester
  * 
- * @author Jaume Llopis <jaume@mnkcoder.com>
+ * @author Coder1 <coder1@mnkcoder.com>
  ******************************************************************************/
 
 //carga de la instancia
 add_action('plugins_loaded',function(){
     if( defined('CODERS_FRAMEWORK_BASE') && class_exists('CodersApp') ){
-        if( CodersApp::create( 'coders-digital-creator' ) ){
-            
+        if( CodersApp::create( __DIR__ ) === false){
+            //message
         }
-        
-        //CodersApp::registerEndPoint('testme', function( \CODERS\Framework\Request $request ){
-        //    var_dump($request);
-        //    return true;
-        //});
     }
 });
-
-
-/**
- * Setup installation/uninstallation hooks
- */
-
 register_activation_hook(__FILE__, function( ){
-
-    //base de datos y activación de dependencias
-    if( defined('CODERS_FRAMEWORK_BASE') && class_exists('CodersApp') ){
-        
-        $installer = CodersApp::installer(__DIR__,'digitor');
-        
-        if( !is_null($installer) &&  $installer->install() ){
-            add_action( 'admin_notices', function() use( $installer ){
-                foreach( $installer->report() as $message ){
-                    printf('<div class="notice notice-success is-dismissible"><p>%s</p></div>',
-                        __($message,'coders_digital_creator'));
-                }
-            } );
-        }
+    if( class_exists('CodersApp') ){
+        CodersApp::__install(__DIR__, function($endpoint,$key){
+            return TRUE;
+        }, 'digitor');
     }
 });
-
 register_deactivation_hook(__FILE__, function(){
-    //base de datos y activación de dependencias
-    if( defined('CODERS_FRAMEWORK_BASE') && class_exists('CodersApp') ){
-        
-        $installer = CodersApp::installer(__DIR__,'digitor');
-
-        if( !is_null($installer) &&  $installer->uninstall() ){
-            add_action( 'admin_notices', function() use( $installer ){
-                var_dump($installer);
-                foreach( $installer->report() as $message ){
-                    printf('<div class="notice notice-success is-dismissible"><p>%s</p></div>',
-                        __($message,'coders_digital_creator'));
-                }
-            } );
-        }
+    if( class_exists('CodersApp') ){
+        CodersApp::__uninstall(__DIR__, function ( $endpoint , $key ){
+            return TRUE;
+        },'digitor');
     }
 });
 
